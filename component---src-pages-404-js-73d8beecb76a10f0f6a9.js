7965256be04c3ae18a4f221fe9e23a24eb627133
Michael Hadley <michaelwesthadley@gmail.com>
2017-10-20 15:46:39 -0500

    Updates

---
+++ component---src-pages-404-js-73d8beecb76a10f0f6a9.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x9427c64ab85d],{228:function(e,t,a){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var l=a(2),u=n(l),c=function(){return u.default.createElement("div",{className:"container"},u.default.createElement("section",{className:"section"},u.default.createElement("div",{className:"content is-medium"},u.default.createElement("h1",null,"No such page found!"),u.default.createElement("p",null,"Much sadness :("))))};t.default=c,e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-404-js-73d8beecb76a10f0f6a9.js.map
