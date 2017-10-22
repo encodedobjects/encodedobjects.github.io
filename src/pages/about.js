749a7781d47b802c4adc12290c6af0dc84fb0fae
Michael Hadley <michaelwesthadley@gmail.com>
2017-10-22 12:25:33 -0500

    Adding bios to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,6 +18,26 @@ const About = () => (
           integrate their varied experiences by merging the physical and digital
           realms into singular works of art.
         </p>
+        <h1>Artist Bios</h1>
+        <p>
+          Mike Hadley is an artist, software developer, and educator based out
+          of Chicago, IL. Mike works and teaches at the intersection of art,
+          technology and science. With training in both Art and Computational
+          Neuroscience, he supports his art practice with a mixture of critical
+          & technological skills. Mike is interested in revealing hidden systems
+          and probing their implications - whether it is the process of genetic
+          engineering caffeine into food or excavating the heated arguments
+          between Wikipedia editors.
+        </p>
+        <p>
+          Jonathan Rockford lives and works in South Florida. With a background
+          in sculpture, ceramics, fine-woodworking, and video art, Jonathan is
+          also intensely interested in the confluence of the physical and
+          digital in our daily lives. When he is not teaching Art and Art
+          History, he can be found listening to audiobooks & podcasts while
+          working in the studio, cultivating his butterfly garden, or lounging
+          with feline friends.
+        </p>
       </div>
     </section>
   </div>
