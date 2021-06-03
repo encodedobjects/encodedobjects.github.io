397d8d1f21615da1d63a7e9755254102e4ae52cc
Michael Hadley <michaelwesthadley@gmail.com>
2021-06-03 11:57:47 -0500

    Gatsby v3 upgrade: add graphql import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "gatsby-link";
+import { graphql } from 'gatsby' 
 
 export default function IndexPage(props) {
   const projects = props.data.allMarkdownRemark.edges;
