2b535bd65f732c30f4a8d776346e5e97b3498ee1
Michael Hadley <michaelwesthadley@gmail.com>
2021-06-03 11:59:28 -0500

    Update formspree contact page to their new api

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,27 +5,15 @@ const About = () => (
     <section className="section">
       <div className="content is-medium">
         <h1>Get in Touch</h1>
-        <p>
-          Interested in our work? Or, maybe you just want to say hello? Drop us
-          a line.
-        </p>
+        <p>Interested in our work? Or, maybe you just want to say hello? Drop us a line.</p>
 
-        <form
-          action="https://formspree.io/encodedobjects@gmail.com"
-          method="POST"
-          className="contact-form"
-        >
+        <form action="https://formspree.io/f/mzbyrkbz" method="POST" className="contact-form">
           <div className="field">
             <label htmlFor="contact-name" className="label">
               Your Name
             </label>
             <div className="control">
-              <input
-                id="contact-name"
-                className="input"
-                name="name"
-                type="text"
-              />
+              <input id="contact-name" className="input" name="name" type="text" />
             </div>
           </div>
 
@@ -34,12 +22,7 @@ const About = () => (
               Your Email
             </label>
             <div className="control">
-              <input
-                id="contact-email"
-                className="input"
-                name="email"
-                type="email"
-              />
+              <input id="contact-email" className="input" name="email" type="email" />
             </div>
           </div>
 
@@ -48,12 +31,7 @@ const About = () => (
               Your Message
             </label>
             <div className="control">
-              <textarea
-                className="textarea"
-                name="message"
-                rows="4"
-                id="contact-message"
-              />
+              <textarea className="textarea" name="message" rows="4" id="contact-message" />
             </div>
           </div>
 
@@ -62,14 +40,6 @@ const About = () => (
               <input type="submit" className="button" value="Send →" />
             </div>
           </div>
-
-          <input type="text" name="_gotcha" style={{ display: "none" }} />
-          <input type="hidden" name="_subject" value="Portfolio Contact" />
-          {/* <input
-                type="hidden"
-                name="_next"
-                value="//encodedobjects.io/contact.html?contact=1"
-              /> */}
         </form>
       </div>
     </section>
